2932cdca6d2c5f2869bac061cd41091a11973402
Candice Parker <lexus2424@gmail.com>
2019-07-25 21:38:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,3 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-(1,"Mr. Chocolate", 25, "M", "brown", "calm", 1)
+(1,"Mr. Chocolate", 25, "M", "brown", "calm", 1),
+(2, "Rowdy", 15, "M", "black", "anxious", 1),
